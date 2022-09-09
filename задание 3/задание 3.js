4c41114b1ca733a751533bb45c30092b12f24760
Andrew <malinaenglishclub@gmail.com>
2022-09-09 22:13:33 +0300

    final work done and gif loaded

--- задание 3/задание 3.js
+++ задание 3/задание 3.js
@@ -41,3 +41,25 @@ btnSend.addEventListener("click", () => {
   websocket.send(message);
   inputValue.value = "";
 });
+
+var geo = document.getElementById("geo");
+geo.addEventListener("click", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    output.innerHTML = "Геолокация не поддерживается в этом браузере";
+  }
+});
+
+function showPosition(position) {
+  let coord = document.createElement("p");
+  coord.classList.add("speaker");
+  coord.innerHTML = `<a style="text-decoration:none; color:blue;" target='_blank' href='https://www.openstreetmap.org/search?whereami=1&query=
+   ${position.coords.latitude} +%2C${position.coords.longitude}#map=19/${position.coords.latitude}/${position.coords.longitude}'>
+   Гео-локация</a>`;
+  output.appendChild(coord);
+}
+
+localStorage.setItem("chatArr", JSON.stringify(chatArr));
+
+JSON.parse(localStorage.getItem("chatArr"));
